e612528bda94e19efe2bb30ff33c395ba0167151
Koxypo <onemorekorn@gmail.com>
2024-05-17 18:35:29 +0500

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-    
+    <header className='app-header'>
+      <h1>Book library app</h1>
+    </header>
     </div>
   );
 }
